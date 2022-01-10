078d308477edb9df8612caee98c1414ba75fedb3
Cheng Gang <chenggangpro@gmail.com>
2022-01-10 16:11:14 +0800

    update test

--- mybatis-r2dbc/src/test/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/suite/support/R2dbcConnectionFactoryProperties.java
+++ mybatis-r2dbc/src/test/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/suite/support/R2dbcConnectionFactoryProperties.java
@@ -4,6 +4,7 @@ import io.r2dbc.spi.ValidationDepth;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.util.ClassUtils;
 import reactor.util.annotation.Nullable;
 
 import java.io.UnsupportedEncodingException;
@@ -32,11 +33,6 @@ public class R2dbcConnectionFactoryProperties {
      * Whether to generate a random connection factory name.
      */
     private boolean generateUniqueName = true;
-
-    /**
-     * Jdbc format URL .
-     */
-    private String jdbcUrl;
 
     /**
      * R2dbc format Url
@@ -79,12 +75,8 @@ public class R2dbcConnectionFactoryProperties {
      * @return
      */
     public String determineConnectionFactoryUrl(){
-        if(!hasText(this.jdbcUrl) && !hasText(this.r2dbcUrl)){
+        if (!hasText(this.r2dbcUrl)) {
             return null;
-        }
-        if(hasText(this.r2dbcUrl)){
-            this.r2dbcUrl = this.r2dbcUrl.replace("r2dbc:mysql:", "r2dbc:mariadb:");
-            return this.r2dbcUrl;
         }
         String encodedUsername;
         try {
@@ -100,8 +92,12 @@ public class R2dbcConnectionFactoryProperties {
             //fallback to original password
             encodedPassword = password;
         }
-        String credential =encodedUsername + (password == null || password.isEmpty() ? "" : ":" + encodedPassword);
-        this.r2dbcUrl = this.jdbcUrl.replace("r2dbc:mysql:", "r2dbc:mariadb:");
+        String credential = encodedUsername + (password == null || password.isEmpty() ? "" : ":" + encodedPassword);
+        //only replace 'r2dbc:mysql:' with 'r2dbc:mariadb' when using 'MariadbConnectionFactory'
+        boolean isMariadbConnectionfactoryPresent = ClassUtils.isPresent("org.mariadb.r2dbc.MariadbConnectionFactory", this.getClass().getClassLoader());
+        if (isMariadbConnectionfactoryPresent && this.r2dbcUrl.startsWith("r2dbc:mysql:")) {
+            this.r2dbcUrl = this.r2dbcUrl.replace("r2dbc:mysql:", "r2dbc:mariadb:");
+        }
         this.r2dbcUrl = r2dbcUrl.replace("//", "//" + credential + "@");
         return this.r2dbcUrl;
     }
