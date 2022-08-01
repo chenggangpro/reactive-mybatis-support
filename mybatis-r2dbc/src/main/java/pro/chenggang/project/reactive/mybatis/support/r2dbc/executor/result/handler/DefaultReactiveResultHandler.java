dbd1f85828aedf5249ff825f914c2c35a2810429
Gang Cheng <chenggangpro@gmail.com>
2022-08-01 16:54:13 +0800

    fix: #86

--- mybatis-r2dbc/src/main/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/executor/result/handler/DefaultReactiveResultHandler.java
+++ mybatis-r2dbc/src/main/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/executor/result/handler/DefaultReactiveResultHandler.java
@@ -437,12 +437,14 @@ public class DefaultReactiveResultHandler implements ReactiveResultHandler {
         if (constructors.length == 1) {
             return Optional.of(constructors[0]);
         }
-        for (final Constructor<?> constructor : constructors) {
-            if (constructor.isAnnotationPresent(AutomapConstructor.class)) {
-                return Optional.of(constructor);
-            }
-        }
-        if (r2DbcMybatisConfiguration.isArgNameBasedConstructorAutoMapping()) {
+        Optional<Constructor<?>> annotated = Arrays.stream(constructors)
+                .filter(x -> x.isAnnotationPresent(AutomapConstructor.class))
+                .reduce((x, y) -> {
+                    throw new ExecutorException("@AutomapConstructor should be used in only one constructor.");
+                });
+        if (annotated.isPresent()) {
+            return annotated;
+        } else if (r2DbcMybatisConfiguration.isArgNameBasedConstructorAutoMapping()) {
             // Finding-best-match type implementation is possible,
             // but using @AutomapConstructor seems sufficient.
             throw new ExecutorException(MessageFormat.format(
