3a35390cffb9e5f5871b392b14859b47779d60a7
Gang Cheng <chenggangpro@gmail.com>
2022-08-02 10:36:33 +0800

    fix: #88 (#91)
    fix: #88

--- mybatis-r2dbc-spring/src/main/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/spring/annotation/R2dbcMapperScannerRegistrar.java
+++ mybatis-r2dbc-spring/src/main/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/spring/annotation/R2dbcMapperScannerRegistrar.java
@@ -83,8 +83,6 @@ public class R2dbcMapperScannerRegistrar implements ImportBeanDefinitionRegistra
         }
 
         List<String> basePackages = new ArrayList<>();
-        basePackages.addAll(Arrays.stream(annoAttrs.getStringArray("value")).filter(StringUtils::hasText).collect(Collectors.toList()));
-
         basePackages.addAll(Arrays.stream(annoAttrs.getStringArray("basePackages")).filter(StringUtils::hasText).collect(Collectors.toList()));
 
         basePackages.addAll(Arrays.stream(annoAttrs.getClassArray("basePackageClasses")).map(ClassUtils::getPackageName).collect(Collectors.toList()));
