a8efb15e8986f07a6e470aee41887e1f9d1bc529
Gang Cheng <chenggangpro@gmail.com>
2022-08-02 10:36:59 +0800

    fix: #89 (#92)
    fix: #89

--- mybatis-r2dbc-spring/src/main/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/spring/configuration/R2dbcMybatisAutoConfiguration.java
+++ mybatis-r2dbc-spring/src/main/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/spring/configuration/R2dbcMybatisAutoConfiguration.java
@@ -37,7 +37,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
-import org.springframework.core.NestedIOException;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -247,7 +246,7 @@ public class R2dbcMybatisAutoConfiguration {
                                 r2dbcMybatisConfiguration, mapperLocation.toString(), r2dbcMybatisConfiguration.getSqlFragments());
                         xmlMapperBuilder.parse();
                     } catch (Exception e) {
-                        throw new NestedIOException("Failed to parse mapping resource: '" + mapperLocation + "'", e);
+                        throw new IOException("Failed to parse mapping resource: '" + mapperLocation + "'", e);
                     } finally {
                         ErrorContext.instance().reset();
                     }
