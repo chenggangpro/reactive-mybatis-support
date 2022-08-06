8b2b49119efef638034124d48289c4b10077c992
Gang Cheng <chenggangpro@gmail.com>
2022-08-06 18:14:46 +0800

    refactor sql placeholder replace method in DefaultPlaceholderFormatter

--- mybatis-r2dbc/src/main/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/executor/placeholder/defaults/DefaultPlaceholderFormatter.java
+++ mybatis-r2dbc/src/main/java/pro/chenggang/project/reactive/mybatis/support/r2dbc/executor/placeholder/defaults/DefaultPlaceholderFormatter.java
@@ -83,65 +83,56 @@ public class DefaultPlaceholderFormatter implements PlaceholderFormatter {
     protected String formatPlaceholderInternal(PlaceholderDialect placeholderDialect, BoundSql boundSql) {
         String sql = boundSql.getSql();
         char defaultPlaceholder = DEFAULT_PLACEHOLDER.charAt(0);
-        boolean containsAnyPlaceholder = sql.chars().anyMatch(aChar -> aChar == defaultPlaceholder);
-        if (!containsAnyPlaceholder) {
+        if (sql.indexOf(defaultPlaceholder) < 0) {
             if (log.isTraceEnabled()) {
                 log.trace("Placeholder not found ,use original sql");
             }
             return sql;
         }
-        char[] chars = sql.toCharArray();
+        int length = sql.length();
         String marker = placeholderDialect.getMarker();
-        StringBuilder builder = new StringBuilder(sql.length() + 10);
-        if (placeholderDialect.usingIndexMarker()) {
-            int startIndex = placeholderDialect.startIndex();
-            for (int i = 0; i < chars.length; i++) {
-                char aChar = chars[i];
-                if (aChar != defaultPlaceholder) {
-                    builder.append(aChar);
-                    continue;
-                }
-                boolean previousMatched = chars[i - 1] == defaultPlaceholder;
-                boolean forwardMatched = i + 1 < chars.length && chars[i + 1] == defaultPlaceholder;
-                if (previousMatched || forwardMatched) {
-                    builder.append(aChar);
-                    continue;
-                }
-                builder.append(marker).append(startIndex);
-                startIndex++;
-            }
-            String formattedSql = builder.toString();
-            if (log.isDebugEnabled()) {
-                log.debug("Format placeholder based by index, with (" + placeholderDialect.getClass().getSimpleName() + ")");
-                log.debug("Formatted SQL  => " + formattedSql);
-            }
-            return formattedSql;
-        }
         List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
-        int identifierCount = 0;
-        for (int i = 0; i < chars.length; i++) {
-            char aChar = chars[i];
+        int identifierIndex = placeholderDialect.usingIndexMarker() ? placeholderDialect.startIndex() : 0;
+        StringBuilder builder = new StringBuilder(length + 10);
+        int begin = 0;
+        boolean multiMatched = false;
+        for (int i = 0; i < length; i++) {
+            char aChar = sql.charAt(i);
             if (aChar != defaultPlaceholder) {
-                builder.append(aChar);
                 continue;
             }
-            boolean previousMatched = chars[i - 1] == defaultPlaceholder;
-            boolean forwardMatched = i + 1 < chars.length && chars[i + 1] == defaultPlaceholder;
-            if (previousMatched || forwardMatched) {
-                builder.append(aChar);
+            if (i < length - 1 && sql.charAt(i + 1) == defaultPlaceholder) {
+                multiMatched = true;
                 continue;
             }
-            builder.append(marker);
-            String parameterProperty = parameterMappings.get(identifierCount).getProperty().replaceAll("\\.", "_");
-            builder.append(parameterProperty);
-            identifierCount++;
+            if (multiMatched) {
+                builder.append(sql, begin, i + 1);
+                begin = i + 1;
+                multiMatched = false;
+                continue;
+            }
+            if (placeholderDialect.usingIndexMarker()) {
+                builder.append(sql, begin, i).append(marker).append(identifierIndex);
+            } else {
+                String parameterProperty = parameterMappings.get(identifierIndex).getProperty()
+                        .replaceAll("\\.", "_");
+                builder.append(sql, begin, i).append(marker).append(parameterProperty);
+            }
+            identifierIndex++;
+            begin = i + 1;
         }
-        String formattedSql = builder.toString();
+        if (begin != length) {
+            builder.append(sql, begin, length);
+        }
         if (log.isDebugEnabled()) {
-            log.debug("Format placeholder based on parameter name, with (" + placeholderDialect.getClass().getSimpleName() + ")");
-            log.debug("Formatted SQL  => " + formattedSql);
+            if (placeholderDialect.usingIndexMarker()) {
+                log.debug("Format placeholder based by index, with (" + placeholderDialect.getClass().getSimpleName() + ")");
+            } else {
+                log.debug("Format placeholder based on parameter name, with (" + placeholderDialect.getClass().getSimpleName() + ")");
+            }
+            log.debug("Formatted SQL  => " + builder);
         }
-        return formattedSql;
+        return builder.toString();
     }
 
 }
